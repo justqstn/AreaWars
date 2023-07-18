f39f3103b1611da2d3999badb8cfe743ddee0888
justqstn <128418414+justqstn@users.noreply.github.com>
2023-07-18 20:01:04 +0300

    Update client.js

--- client.js
+++ client.js
@@ -542,7 +542,7 @@ clearing_timer.OnTimer.Add(function () {
 		if (array_areas == null) array_areas = GetAreas();
 		
 		for (let i = array_areas.length - 1; i > array_areas.length - 1; i--) {
-			let area = AreaService.Get(array_areas[i]);
+			
 			
 			array_areas.pop()
 		}
