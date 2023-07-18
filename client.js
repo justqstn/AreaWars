91304110b2ee677bf359d2cbb2b267aacd7892ba
justqstn <128418414+justqstn@users.noreply.github.com>
2023-07-18 18:35:59 +0300

    Update client.js

--- client.js
+++ client.js
@@ -288,7 +288,10 @@ function prd_Vests(level, cost) {
 	this.Cost = cost;
 	this.Currency = "silver";
 	this.Conditions = function (p) { return !p.contextedProperties.MaxHp.Value < VESTS_VALUE[level - 1]; }
-	this.Buy = function (p) { p.contextedProperties.MaxHp.Value = VESTS_VALUE[level - 1]; }
+	this.Buy = function (p) { 
+		p.contextedProperties.MaxHp.Value = VESTS_VALUE[level - 1]; 
+		p.Spawns.Spawn();
+	}
 }
 
 function prd_MaxPoints(plus, limit, cost, currency) {
@@ -332,7 +335,7 @@ function prd_Exp(plus, cost, currency) {
 }
 
 const Products = [
-	new prd_Inventory("Build", 1000, "silver"), new prd_Inventory("Secondary", 9000, "silver"), new prd_Inventory("Main", 2.25, "gold"), new prd_Regen(10, 15000, "silver"), new prd_Regen(30, 7, "gold"),
+	new prd_Inventory("Build", 500, "silver"), new prd_Inventory("Secondary", 3000, "silver"), new prd_Inventory("Main", 2.25, "gold"), new prd_Regen(10, 15000, "silver"), new prd_Regen(30, 7, "gold"),
 	new prd_Saves("save_silver", 50000, "silver"), new prd_Saves("save_gold", 40, "gold"), new prd_Vests(1, 12000), new prd_Vests(2, 50000), new prd_Vests(3, 82000), new prd_MaxPoints(5, 150, 3, "gold"), new prd_MaxPoints(15, 150, 8, "gold"),
 	new prd_Boosters("silver_booster", 0.25, 3, 20000, "silver"), new prd_Boosters("silver_booster", 0.5, 3, 3, "gold"), new prd_Boosters("gold_booster", 0.1, 2, 50000, "silver"), new prd_Boosters("gold_booster", 0.2, 2, 7, "gold"),
 	{
@@ -625,7 +628,7 @@ function SecondPhase() {
 	Ui.GetContext().Hint.Value = "Фаза 2";
 	state.Value = "second";
 
-	updateTimer.RestartLoop(1);
+	update_timer.RestartLoop(1);
 	main_timer.Restart(1200);
 }
 
